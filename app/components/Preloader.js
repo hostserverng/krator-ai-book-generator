1713439c93a4a2fe8038d568f7d1b48ec80f1392
Ash200042123 <ashfaq@iut-dhaka.edu>
2024-03-02 14:33:47 +0600

    loader added

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -3,16 +3,20 @@ import React from "react";
 const Preloader = () => {
   return (
     <div className="preloader">
-      <figure className="kg-card kg-image-card kg-card-hascaption">
-        <img
-          src="https://cdn.svgator.com/images/2023/06/iris-blades-css-spinner.svg"
-          className="kg-image lightense-target bg-transparent"
-          alt="Iris Blades CSS spinner"
-          loading="lazy"
-          width="700"
-          height="400"
-        />
-
+      <figure>
+        <object
+          type="image/svg+xml"
+          data="https://cdn.svgator.com/images/2023/06/generative-ai-preloader.svg"
+          alt="Generative AI preloader example"
+          img=""
+          width="100%"
+        >
+          <img
+            src="https://cdn.svgator.com/images/2023/06/generative-ai-preloader.svg"
+            alt="Generative AI preloader example"
+            className="lightense-target"
+          />
+        </object>
       </figure>
     </div>
   );
